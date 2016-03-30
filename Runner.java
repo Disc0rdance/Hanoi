8b325670450cdf360d13e882664aef923d54af09
Disc0rdance <s.adameichhorn@gmail.com>
2016-03-30 12:20:40 -0700

    Done

--- Runner.java
+++ Runner.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.lang.Math;
 /**
  * Write a description of class Runner here.
  * 
@@ -9,6 +10,7 @@ public class Runner {
     private Tower t1, t2, t3;
     private int numDiscs;
     private int count;
+    private double moves;
     
     public void main(int n) {
         t1 = new Tower();
@@ -17,38 +19,23 @@ public class Runner {
         
         numDiscs = n;
         count = 0;
+        moves = (Math.pow(2, numDiscs))-1;
+        
         t1.fillTower(numDiscs);
+        
+        sort(numDiscs, "Tower 1", "Tower 2", "Tower 3");
+        System.out.println(moves);
     }
     
-    public void sort1(int n) {
-        if (n % 2 == 0) {
-            t2.addDisc(t1.removeDisc());
-            t3.addDisc(t1.removeDisc());
-            t3.addDisc(t2.removeDisc());
-            count += 3;
+    public void sort(int topDisc, String tower1, String tower2, String tower3) {
+        if (topDisc == 1) {
+            System.out.println("Disc 1 from " + tower1 + " to " + tower3);
         }
         else {
-            t2.addDisc(t1.removeDisc());
-            t1.addDisc(t3.removeDisc());
-            t2.addDisc(t3.removeDisc());
-            count += 3;
+            sort(topDisc-1, tower1, tower3, tower2);
+            System.out.println("Disc " + topDisc + " from " + tower1 + " to " + tower3);
+            sort(topDisc-1, tower2, tower1, tower3);
         }
-        sort1(n-1);
-    }
-    
-    public void sort2(int n) {
-        if (n % 2 != 0) {
-            t3.addDisc(t1.removeDisc());
-            t2.addDisc(t1.removeDisc());
-            t2.addDisc(t3.removeDisc());
-            count += 3;
-        }
-        else {
-            t3.addDisc(t1.removeDisc());
-            t1.addDisc(t2.removeDisc());
-            t3.addDisc(t2.removeDisc());
-            count += 3;
-        }
-        sort2(n-1);
+        
     }
 }
